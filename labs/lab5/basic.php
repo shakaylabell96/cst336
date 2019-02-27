de7ffba63b849a8bacd2f16a49ec062927dd273d
Shakayla Bell <shbell@csumb.edu>
2019-02-27 01:36:38 +0000

    adding homework 2

---
+++ labs/lab5/basic.php
@@ -0,0 +1,12 @@
+<?php
+
+$otterId = "good9016";
+echo "My otter id is: " . $otterId;
+
+$newArray = array("one", "two", "three");
+
+$weekdayys = array();
+
+$weekdays[]="Monday";
+$weekdays[]="Tuesday";
+array_push($week)
